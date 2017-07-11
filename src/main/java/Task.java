f164e16229036f844ae074b171aaf6fa02590752
Alena Golovina and Esvethlynna Pangelinan <esvethlynna@>
2017-07-11 13:28:44 -0700

    add functionality to delete tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -87,5 +87,14 @@ public class Task {
   }
 }
 
+public void delete() {
+  try(Connection con = DB.sql2o.open()) {
+  String sql = "DELETE FROM tasks WHERE id = :id;";
+  con.createQuery(sql)
+    .addParameter("id", id)
+    .executeUpdate();
+  }
+}
+
 
 }
